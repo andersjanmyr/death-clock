5b018769dbfd8c48ef41ceffacbc7c0ad28849d9
Anders Janmyr <anders@janmyr.com>
2014-03-16 10:07:53 +0100

    Added server and a route

---
+++ server.js
@@ -0,0 +1,60 @@
+'use strict';
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var quotes = require('./lib/routes/quotes');
+
+var debug = require('debug')('death-clock');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.set('env', process.env.NODE_ENV || 'development');
+app.use(logger('dev'));
+app.use(bodyParser());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get('/status', function(req, resp) {
+    resp.send('To be or not to be, that is the question');
+});
+
+app.use('/quotes', quotes);
+
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log(app.get('env') + ' server listening on port ' + app.get('port'));
+});
+
+module.exports = app;
