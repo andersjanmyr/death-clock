9b635cf6dce1a17f61313397d602a6f0a28fcf39
Anders Janmyr <anders@janmyr.com>
2014-03-23 11:53:00 +0100

    Try to check for changes

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,7 +7,9 @@ git checkout deploy
 git merge master --no-edit
 npm run build.js
 npm run build-less
-git commit -am 'New deploy'
+if $(git commit -am 'New deploy'); then
+  echo 'Changes Committed'
+fi
 git push heroku deploy:master
 git checkout master
 git stash pop
