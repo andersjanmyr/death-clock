55dd805ca0f5b6c7f0d5b6d694957ad4baa13bfa
Anders Janmyr <anders@janmyr.com>
2016-10-15 13:46:27 -0700

    Added heroku deploy

---
+++ bin/deploy-heroku.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+
+git stash save 'Before deploy'
+git checkout deploy
+git merge master --no-edit
+npm run build
+if git commit -am Deploy; then
+  echo 'Changes Committed'
+fi
+git push heroku deploy:master
+git checkout master
+git stash pop
