3bd44b8e4958add1a28e302896317a30df97492f
Anders Janmyr <anders@janmyr.com>
2017-01-08 21:33:58 -0800

    Deploy typo

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -5,7 +5,7 @@ set -o errexit
 git stash save 'Before deploy'
 git checkout deploy
 git merge master --no-edit
-npm run build.js
+npm run build-js
 npm run build-less
 if git commit -am Deploy; then
   echo 'Changes Committed'
